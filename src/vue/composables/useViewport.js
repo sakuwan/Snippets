2e9318f9089692004cd7f977ad5ab9dad52b5560
Wan <wanwebdev@gmail.com>
2021-08-14 16:12:56 -0400

    Update -- useViewport
    Updated useViewport to be more optimized for multiple usages -- no more multiple base window resize listeners

--- src/vue/composables/useViewport.js
+++ src/vue/composables/useViewport.js
@@ -1,7 +1,8 @@
-import { ref, onBeforeUnmount } from 'vue';
+import { reactive, toRefs, onBeforeUnmount } from 'vue';
 
 import { useEvent } from './useEvent';
 
+import { isDefined } from '../utils/isDefined';
 import { passiveSupported } from '../utils/passiveSupported';
 import { wrapEventListener } from '../utils/wrapEventListener';
 
@@ -12,28 +13,65 @@ const defaultOptions = Object.freeze({
   eventOpts: passiveSupported() ? { passive: true } : false,
 });
 
+const eventState = {
+  count: 0,
+  removeHook: null,
+  subscribers: new Map(),
+};
+
+const viewportState = reactive({
+  viewportWidth: window.innerWidth,
+  viewportHeight: window.innerHeight,
+});
+
 // eslint-disable-next-line import/prefer-default-export
-export const useViewport = (onViewportChange = null, options = {}) => {
-  const { eventOpts, ...wrapperOpts } = { ...defaultOptions, ...options };
+export const useViewport = (options = {}, onViewportChange = null) => {
+  // Check if this is the first use of the composable, attach window listener
+  const isEventAttached = isDefined(eventState.removeHook);
+  if (!isEventAttached) {
+    const { eventOpts, ...wrapperOpts } = { ...defaultOptions, ...options };
 
-  const viewportWidth = ref(window.innerWidth);
-  const viewportHeight = ref(window.innerHeight);
+    const resizeListener = wrapEventListener((event) => {
+      // Invoke all of the valid resize subscribers
+      eventState.subscribers.forEach((_, fn) => { fn(event); });
 
-  const resizeListener = wrapEventListener((event) => {
-    onViewportChange?.(event);
+      viewportState.viewportWidth = window.innerWidth;
+      viewportState.viewportHeight = window.innerHeight;
+    }, wrapperOpts);
 
-    viewportWidth.value = window.innerWidth;
-    viewportHeight.value = window.innerHeight;
-  }, wrapperOpts);
+    eventState.removeHook = useEvent(window, 'resize', resizeListener, eventOpts);
+  }
 
-  const removeHook = useEvent(window, 'resize', resizeListener, eventOpts);
+  // Store any valid resize subscribers by key
+  if (isDefined(onViewportChange)) {
+    eventState.subscribers.set(onViewportChange, true);
+  }
 
-  onBeforeUnmount(() => { removeHook(); });
+  // Remove subscription from subscriber map
+  const unsubscribe = () => {
+    const { subscribers } = eventState;
+    if (subscribers.has(onViewportChange)) {
+      subscribers.delete(onViewportChange);
+    }
+  };
+
+  // Decrease the overall use count, removing the event hook if at zero
+  onBeforeUnmount(() => {
+    eventState.count -= 1;
+    if (eventState.count === 0) {
+      eventState.removeHook();
+      eventState.removeHook = null;
+    }
+
+    unsubscribe();
+  });
+
+  // Increase the overall use count
+  eventState.count += 1;
 
   return {
-    viewportWidth,
-    viewportHeight,
+    ...toRefs(viewportState),
 
-    removeHook,
+    removeHook: unsubscribe,
   };
 };
