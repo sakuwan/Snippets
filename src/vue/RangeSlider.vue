1cc56178a67a6cabcbaf52cbb03d26516c89cb6d
Wan <wanwebdev@gmail.com>
2021-08-26 00:01:49 -0400

    Component -- RangeSlider
    Quick wrapper over a range input

---
+++ src/vue/RangeSlider.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+
+input(type="range" v-bind="$attrs" v-model.number="localValue")
+
+</template>
+
+<script>
+
+import { computed } from 'vue';
+
+import { makeSingleTypedProps } from './utils/propHelpers';
+
+export default {
+  emits: ['update:modelValue'],
+  props: { ...makeSingleTypedProps({ modelValue: 0 }) },
+
+  setup(props, { emit }) {
+    const localValue = computed(({
+      get: () => props.modelValue,
+      set: (v) => emit('update:modelValue', v),
+    }));
+
+    return { localValue };
+  },
+};
+
+</script>
